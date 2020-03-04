a8e45e32043639c019c1a3ebf5a894bf3bb9cbbe
anthonyc_36 <anthonychristy78@gmail.com>
2020-03-04 14:30:12 -0500

    Ya Yeet. My game is beat

---
+++ unity 2d plants vs zombies clone/Assets/Scripts/SunCollect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunCollect : MonoBehaviour {
+    // Global score
+    public static int score = 600;
+
+    void OnMouseDown()
+    {
+        // Increase Score
+        score += 25;
+
+        // Destroy Sun
+        Destroy(gameObject);
+    }
+
+}
